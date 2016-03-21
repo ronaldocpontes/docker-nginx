8356d16f7c018e1868d1691130d4877ce45a88c1
Ronaldo Pontes <EMAIL>
2016-03-21 08:15:54 +0000

    templating

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM nginx:1.9.9
-MAINTAINER Mark Shust <mark.shust@mageinferno.com>
+#MAINTAINER Mark Shust <mark.shust@mageinferno.com>
 
 ENV PHP_HOST php-fpm
 ENV PHP_PORT 9000
@@ -7,7 +7,6 @@ ENV APP_MAGE_MODE default
 
 WORKDIR /src
 
-COPY ./default.conf /etc/nginx/conf.d/
 COPY ./start.sh /usr/local/bin/start.sh
 
 CMD ["/usr/local/bin/start.sh"]
--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+cp /etc/nginx/templates/* /etc/nginx/conf.d/
+
 [ ! -z "${VIRTUAL_HOST}" ] && sed -i "s/VIRTUAL_HOST/${VIRTUAL_HOST}/" /etc/nginx/conf.d/default.conf
 [ ! -z "${PHP_HOST}" ] && sed -i "s/PHP_HOST/${PHP_HOST}/" /etc/nginx/conf.d/default.conf
 [ ! -z "${PHP_PORT}" ] && sed -i "s/PHP_PORT/${PHP_PORT}/" /etc/nginx/conf.d/default.conf
